29bb163ebef53a0eea1bbb166727708472539e1e
Suman Komarla Adinarayana <suman.groups@gmail.com>
2025-11-05 00:26:23 +0530

    Create database and table for GitHub projects
    .

--- SumanKAGroups_SampleAIMLProjectInfo/08_SQLBasicsInfo/08_SQLBasicsInfo.sql
+++ SumanKAGroups_SampleAIMLProjectInfo/08_SQLBasicsInfo/08_SQLBasicsInfo.sql
@@ -1,1 +1,15 @@
+CREATE DATABASE SumanKA_SQLdatabase;
 
+CREATE TABLE SKA_Github_OfficialOpenSourceProviders (
+    OrganisationID int,
+    Project varchar(255),
+    Repository1 varchar(255),
+    Projects1 varchar(255),
+    People varchar(255)
+);
+
+
+ALTER TABLE SKA_Github_OfficialOpenSourceProviders
+ADD  Sponsoring varchar(255);
+
+select * from SKA_Github_OfficialOpenSourceProviders;
