f31f42d7f94369cde1fbcf5c61820efef426f59c
Suman Komarla Adinarayana <suman.groups@gmail.com>
2025-11-05 00:39:25 +0530

    Update 08_SQLBasicsInfo.sql

--- SumanKAGroups_SampleAIMLProjectInfo/08_SQLBasicsInfo/08_SQLBasicsInfo.sql
+++ SumanKAGroups_SampleAIMLProjectInfo/08_SQLBasicsInfo/08_SQLBasicsInfo.sql
@@ -1,5 +1,9 @@
 CREATE DATABASE SumanKA_SQLdatabase;
 
+-- To use this database
+USE SumanKA_SQLdatabase;
+
+
 CREATE TABLE SKA_Github_OfficialOpenSourceProviders (
     OrganisationID int,
     Project varchar(255),
@@ -13,3 +17,17 @@ ALTER TABLE SKA_Github_OfficialOpenSourceProviders
 ADD  Sponsoring varchar(255);
 
 select * from SKA_Github_OfficialOpenSourceProviders;
+
+-- List all databases
+SHOW DATABASES;
+
+-- List all tables in the current database
+SHOW TABLES;
+
+-- Describe table structure
+DESCRIBE SKA_Github_OfficialOpenSourceProviders;
+
+INSERT INTO SKA_Github_OfficialOpenSourceProviders (    OrganisationID ,    Project ,    Repository1,    Projects1 ,    People, Sponsoring )
+VALUES (1, 'Microsoft', 'Microsoft', 'vscode', '4400',1);
+
+SELECT * FROM SKA_Github_OfficialOpenSourceProviders;
